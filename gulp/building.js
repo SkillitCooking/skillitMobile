81ceb9e2a3f1e01ac200caa44370f4bf7635a00a
Dane Bratz <danebratz@gmail.com>
2016-09-05 11:05:36 -0600

    made new gulp build-all task

--- gulp/building.js
+++ gulp/building.js
@@ -16,6 +16,8 @@ var buildDependencies = [
   'build-assets'
 ];
 
+gulp.task('build-all', ['build-templates', 'build-assets']);
+
 gulp.task('build', buildDependencies, function () {
   return gulp.src(paths.dist + '/**/*')
     .pipe($.size({showFiles: true}));
@@ -56,7 +58,7 @@ gulp.task('build-app', ['clean', 'inject-all'], function () {
 });
 
 // copy templates
-gulp.task('build-templates', /*['clean'],*/ function () {
+gulp.task('build-templates', ['build-app'], function () {
   return gulp.src(paths.templates)
   .pipe($.if(options.minify, $.htmlmin({
     removeComments: true,
@@ -69,7 +71,7 @@ gulp.task('build-templates', /*['clean'],*/ function () {
 });
 
 // copy assets, wait for fonts
-gulp.task('build-assets', [/*'clean',*/ 'bower-fonts'], function () {
+gulp.task('build-assets', ['build-app', 'bower-fonts'], function () {
   return gulp.src('app/*/assets/**/*')
     .pipe($.if(options.minify, $.imagemin()))
     .pipe(gulp.dest(paths.dist));
