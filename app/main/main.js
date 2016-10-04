47b87b8cd08cc94d9b25ad90d1c48205c72ac332
Dane Bratz <danebratz@gmail.com>
2016-10-04 09:06:21 -0600

    deploy is

--- app/main/main.js
+++ app/main/main.js
@@ -345,4 +345,41 @@ angular.module('main', [
           lesson: null
         }
       });
+})
+//check for hot updates
+.run(function($ionicPopup, $ionicDeploy, Config) {
+  $ionicDeploy.channel = Config.ENV.CHANNEL;
+  $ionicDeploy.check().then(function(snapshotAvailable) {
+    if(snapshotAvailable) {
+      $ionicDeploy.download().then(function() {
+        //check for current snapshot, delete ones that aren't it
+        $ionicDeploy.info().then(function(curSnapShot) {
+          $ionicDeploy.getSnapshots().then(function(snapshots) {
+            console.log('snapshots', snapshots);
+            for (var i = snapshots.length - 1; i >= 0; i--) {
+              if(snapshots[i] !== curSnapShot.deploy_uuid) {
+                console.log('to delete', curSnapShot.deploy_uuid);
+                $ionicDeploy.deleteSnapshot(snapshots[i]);
+              }
+            }
+          });
+        });
+        $ionicDeploy.extract().then(function() {
+          $ionicPopup.show({
+            title: 'Update Available!',
+            template: 'We just fetched an update - would you like to restart Skillit to use the new features?',
+            buttons: [
+              {text: 'No Thanks'},
+              {
+                text: 'Restart',
+                onTap: function(e) {
+                  $ionicDeploy.load();
+                }
+              }
+            ]
+          });
+        });
+      });
+    }
+  });
 });
