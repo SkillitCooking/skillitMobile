735a2434b019a1499b7fb507ecd6317baf0575de
Dane Bratz <danebratz@gmail.com>
2016-10-19 22:17:13 -0700

    deploy nuance

--- app/main/scripts/deploy.js
+++ app/main/scripts/deploy.js
@@ -7,8 +7,30 @@ angular.module('main')
     if(snapshotAvailable) {
       $ionicDeploy.download().then(function() {
         //check for current snapshot, delete ones that aren't it
-        $ionicDeploy.info().then(function(curSnapShot) {
-          $ionicDeploy.getSnapshots().then(function(snapshots) {
+        $ionicDeploy.getSnapshots().then(function(snapshots) {
+          if(snapshots.length === 0) {
+            //then can assume a first time download
+            $ionicDeploy.extract().then(function() {
+              $ionicDeploy.load();
+            });
+          } else {
+            $ionicDeploy.extract().then(function() {
+              $ionicPopup.show({
+                title: 'Update Available!',
+                template: 'We just fetched an update - would you like to restart Skillit to use the new features?',
+                buttons: [
+                  {text: 'No Thanks'},
+                  {
+                    text: 'Update',
+                    onTap: function(e) {
+                      $ionicDeploy.load();
+                    }
+                  }
+                ]
+              });
+            });
+          }
+          $ionicDeploy.info().then(function(curSnapShot) {
             for (var i = snapshots.length - 1; i >= 0; i--) {
               if(snapshots[i] !== curSnapShot.deploy_uuid) {
                 $ionicDeploy.deleteSnapshot(snapshots[i]);
@@ -16,22 +38,7 @@ angular.module('main')
             }
           });
         });
-        $ionicDeploy.extract().then(function() {
-          $ionicPopup.show({
-            title: 'Update Available!',
-            template: 'We just fetched an update - would you like to restart Skillit to use the new features?',
-            buttons: [
-              {text: 'No Thanks'},
-              {
-                text: 'Restart',
-                onTap: function(e) {
-                  $ionicDeploy.load();
-                }
-              }
-            ]
-          });
-        });
       });
-    }
+    } 
   });
 });
