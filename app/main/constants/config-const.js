63fa217444fa8893753dac4f7c7db20ed8e4529a
mikejlaz <mikejlaz@gmail.com>
2017-01-05 12:51:44 -0800

    show your friends

--- app/main/constants/config-const.js
+++ app/main/constants/config-const.js
@@ -4,9 +4,9 @@ angular.module('main')
   .constant('Config', {
     ENV: {
         /*inject-env*/
-        'SERVER_URL': 'https://skillicookingdevapi.info/api/',
-    'CHANNEL': 'dev',
-    'API_PASSWORD': 'MDm|C7oQQIm&AZyhx4g7m^+uNGqm$7Ctt2-60O&Ek-%0o!NuLT'
+        'SERVER_URL': 'https://skillicookingprodapi.info/api/',
+    'CHANNEL': 'production',
+    'API_PASSWORD': 'sm@34MLPG9L&rWph|YMwcg=&5|R3TMZ!!H+F48ThGFl56E&*RD'
         /*endinject*/
     },
     BUILD : {
@@ -153,8 +153,8 @@ angular.module('main')
     'RECIPES_TAB': 1,
     'FAVORITE': 2
   }).constant('SHARING', {
-    'MESSAGE': 'I just cooked ' + name + ' using Skillit!',
+    'MESSAGE': 'I just used Skillit to cook ' + name + '! #skillingit',
     'IMAGE': 'main/assets/images/skillit-orange-text.png',
     'SUBJECT': 'Skillin\' it',
-    'LINK': 'https://skillitcooking.com'
+    'LINK': 'https://skillitcooking.com/pages/app'
   });
