32424facbb02631621c119f6fff9cfc08a916d37
Dane Bratz <danebratz@gmail.com>
2017-01-12 17:25:03 -0800

    stuff

--- app/main/controllers/intro-slides-ctrl.js
+++ app/main/controllers/intro-slides-ctrl.js
@@ -1,6 +1,6 @@
 'use strict';
 angular.module('main')
-.controller('IntroSlidesCtrl', ['$rootScope', '$scope', '$ionicTabsDelegate', '$ionicNavBarDelegate', 'INTRO_SLIDES', function ($rootScope, $scope, $ionicTabsDelegate, $ionicNavBarDelegate, INTRO_SLIDES) {
+.controller('IntroSlidesCtrl', ['$rootScope', '$scope', '$ionicTabsDelegate', '$ionicNavBarDelegate', '$persist', 'INTRO_SLIDES', function ($rootScope, $scope, $ionicTabsDelegate, $ionicNavBarDelegate, $persist, INTRO_SLIDES) {
 	$scope.introSlides = INTRO_SLIDES;
 	$scope.$on('$ionicView.beforeEnter', function(event, data) {
 		$ionicTabsDelegate.showBar(false);
@@ -8,5 +8,8 @@ angular.module('main')
 	});
   $scope.$on('$ionicView.afterEnter', function(event, data) {
     $rootScope.redrawSlides = true;
+  });
+  $scope.$on('$ionicView.afterLeave', function(event, data) {
+    $persist.set('HAS_SEEN', 'FIRST_OPEN', true);
   });
 }]);
--- app/main/scripts/deploy.js
+++ app/main/scripts/deploy.js
@@ -1,37 +1,43 @@
 'use strict';
 angular.module('main')
 //check for hot updates
-.run(function($ionicPopup, $ionicDeploy, $ionicLoading, Config, LOADING) {
+.run(function($ionicPopup, $ionicDeploy, $ionicLoading, $persist, Config, LOADING) {
   $ionicDeploy.channel = Config.ENV.CHANNEL;
-  $ionicDeploy.getSnapshots().then(function(snapshots) {
-    $ionicDeploy.info().then(function(curSnapShot) {
-      for (var i = snapshots.length - 1; i >= 0; i--) {
-        if(snapshots[i] !== curSnapShot.deploy_uuid) {
-          $ionicDeploy.deleteSnapshot(snapshots[i]);
-        }
-      }
-    });
-    $ionicDeploy.check().then(function(snapshotAvailable) {
-      if(snapshotAvailable) {
-        $ionicLoading.show({
-          template: LOADING.TEMPLATE,
-          noBackdrop: true
-        });
-        $ionicDeploy.download().then(function() {
-            if(snapshots.length === 0) {
-              //then can assume a first time download
-              $ionicDeploy.extract().then(function() {
-                $ionicLoading.hide();
-                $ionicDeploy.load();
-              });
-            } else {
-              $ionicDeploy.extract().then(function() {
-                $ionicLoading.hide();
-                $ionicDeploy.load();
-              });
+  $persist.get('HAS_SEEN', 'FIRST_OPEN', false).then(function(hasSeen) {
+    if(hasSeen) {
+      $ionicDeploy.getSnapshots().then(function(snapshots) {
+        $ionicDeploy.info().then(function(curSnapShot) {
+          for (var i = snapshots.length - 1; i >= 0; i--) {
+            if(snapshots[i] !== curSnapShot.deploy_uuid) {
+              $ionicDeploy.deleteSnapshot(snapshots[i]);
             }
+          }
         });
-      } 
-    });
+        $ionicDeploy.check().then(function(snapshotAvailable) {
+          if(snapshotAvailable) {
+            $ionicLoading.show({
+              template: LOADING.TEMPLATE,
+              noBackdrop: true
+            });
+            $ionicDeploy.download().then(function() {
+                if(snapshots.length === 0) {
+                  //then can assume a first time download
+                  $ionicDeploy.extract().then(function() {
+                    $ionicLoading.hide();
+                    $ionicDeploy.load();
+                  });
+                } else {
+                  $ionicDeploy.extract().then(function() {
+                    $ionicLoading.hide();
+                    $ionicDeploy.load();
+                  });
+                }
+            });
+          } 
+        });
+      });
+    } else {
+      $persist.set('HAS_SEEN', 'FIRST_OPEN', true);
+    }
   });
 });
